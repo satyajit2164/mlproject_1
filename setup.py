2d6909e0fb2a96083b6b465c2b57d5213dbace67
satyajit2164 <satyajit2164@gmail.com>
2026-01-02 08:48:50 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mlproject_1',
+    version='0.0.1',
+    author='satyajit2164',
+    author_email='satyajit2164@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+    
+)
